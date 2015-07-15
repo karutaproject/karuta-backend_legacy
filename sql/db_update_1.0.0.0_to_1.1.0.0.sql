84a0e552a6a64824ac0ab3de29edebf41d6719f5
Nobry OUK <o.p.nobry@gmail.com>
2015-07-15 10:29:49 -0400

    Database update script from Karuta 1.0.0.0 to 1.1.0.0

---
+++ sql/db_update_1.0.0.0_to_1.1.0.0.sql
@@ -0,0 +1,25 @@
+-- Updating tables from Karuta 1.0.0.0 to Karuta 1.1.0.0
+
+USE `karuta-backend`;
+
+-- credential_group might have been used, better be safe and just alter it
+ALTER TABLE credential_group MODIFY COLUMN `cg` bigint NOT NULL AUTO_INCREMENT;
+ALTER TABLE credential_group ADD COLUMN `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL;
+
+-- Unused before, no problem dropping it
+DROP TABLE `portfolio_group`;
+CREATE TABLE IF NOT EXISTS `portfolio_group` (
+  `owner` bigint(20) NOT NULL,
+  `portfolio_id` binary(16) NOT NULL,
+  `group_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`portfolio_id`,`group_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE `file_table`;
+DROP TABLE `definition_info`;
+DROP TABLE `definition_type`;
+DROP TABLE `rule_info`;
+DROP TABLE `rule_table`;
+-- Not used, will be removed next time
+-- isCompleteShare still fail gracefully
+-- DROP TABLE `complete_share`;
