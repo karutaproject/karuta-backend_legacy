2475e2beb97c20234336492dedb70d240c48a5ac
Nobry OUK <o.p.nobry@gmail.com>
2019-08-05 17:21:01 -0400

    * Changed SQL DB creation file as test. 	. Next step is arranging this into a script

--- sql/karuta-backend-account.sql
+++ sql/karuta-backend-account.sql
@@ -3,12 +3,34 @@
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-GRANT USAGE ON * . * TO 'karuta'@'localhost' IDENTIFIED BY 'karuta_password' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+SET @user := 'karuta';
+SET @db := 'karuta-backend';
+SET @pass := 'karuta-password';
 
-CREATE DATABASE IF NOT EXISTS `karuta-backend` ;
+SET @sql = CONCAT("CREATE USER '",@user,"'@localhost IDENTIFIED BY '",@pass,"'");
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
-GRANT ALL PRIVILEGES ON `karuta-backend` . * TO 'karuta'@'localhost';
+SET @sql = CONCAT("CREATE DATABASE IF NOT EXISTS `",@db,"`");
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
+SET @sql = CONCAT("GRANT USAGE ON `",@db,"` . * TO '",@user,"'@localhost IDENTIFIED BY '",@pass,"' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0");
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
-USE `karuta-backend` ;
+SET @sql = CONCAT("GRANT ALL PRIVILEGES ON `",@db,"` . * TO '",@user,"'@localhost");
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
+CREATE USER @user @ 'localhost' IDENTIFIED BY @pass;
+
+CREATE DATABASE IF NOT EXISTS @db ;
+
+GRANT USAGE ON @db . * TO @user @ 'localhost' IDENTIFIED BY @pass WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+
+GRANT ALL PRIVILEGES ON @db . * TO @user @ 'localhost';
