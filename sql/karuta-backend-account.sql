a884c43e87c384000fe30034bba91c80962c61f8
Nobry OUK <o.p.nobry@gmail.com>
2020-07-10 16:23:37 -0400

    * Making helper script more general

--- sql/karuta-backend-account.sql
+++ sql/karuta-backend-account.sql
@@ -27,10 +27,18 @@ PREPARE stmt FROM @sql;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
-CREATE USER @user @ 'localhost' IDENTIFIED BY @pass;
+SET @sql = CONCAT("CREATE USER '",@user,"' @ 'localhost' IDENTIFIED BY '",@pass,"'");
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
-CREATE DATABASE IF NOT EXISTS @db ;
+SET @sql = CONCAT("CREATE DATABASE IF NOT EXISTS `",@db,"`");
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
-GRANT USAGE ON @db . * TO @user @ 'localhost' IDENTIFIED BY @pass WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
 
-GRANT ALL PRIVILEGES ON @db . * TO @user @ 'localhost';
+SET @sql = CONCAT("GRANT ALL ON `",@db,"` . * TO '",@user,"' @ 'localhost'")";
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
