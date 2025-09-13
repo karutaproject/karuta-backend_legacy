b00e5c8a5dc9633c0255b7c9be9d8ddcb498fbc2
Nobry OUK <o.p.nobry@gmail.com>
2025-09-12 23:58:30 -0400

    * Have direct url date cap be unlimited unless variable "Cap_Direct_Duration" is set, it's 720 hours otherwise

--- src/com/eportfolium/karuta/data/attachment/DirectURLService.java
+++ src/com/eportfolium/karuta/data/attachment/DirectURLService.java
@@ -76,6 +76,7 @@ public class DirectURLService extends HttpServlet {
 	ArrayList<String> ourIPs = new ArrayList<>();
 
 	private String secretkey;
+	private boolean capDuration;
 
 	public static String hexToString(byte[] bytes) {
 		final var hexchars = new StringBuilder(bytes.length * 2);
@@ -117,6 +118,8 @@ public class DirectURLService extends HttpServlet {
 				}
 			}
 			secretkey = ConfigUtils.getInstance().getRequiredProperty("directkey");
+			// Cap at 720 hours if set
+			capDuration = ConfigUtils.getInstance().getBooleanProperty("Cap_Direct_Duration");
 		} catch (final Exception e) {
 			logger.error("Can't init servlet", e);
 			throw new ServletException(e);
@@ -288,14 +291,14 @@ public class DirectURLService extends HttpServlet {
 				/// Prevent sharing with another personal account after being evaluated.
 				/// Since the specific group will be the username (specific rights),
 				/// we can also know if student tried sharing it with self first
-
+				
 				///// Check if this user is not giving rights to self (existing user account)
-
+				
 				///// Check if user has some access to this uuid
 				/// Prevent somebody else to share another student node
-
+				
 				///// Check if user has right to share
-
+				
 				//// Put person in specified group
 				//*/
 
@@ -483,7 +486,7 @@ public class DirectURLService extends HttpServlet {
 			var durationInt = Integer.parseInt(duration);
 			if (durationInt < 1) {
 				durationInt = 1;
-			} else if (durationInt > 24 * 30) { // 720 hours, 30 days
+			} else if (capDuration && durationInt > 24 * 30) { // 720 hours, 30 days
 				durationInt = 24 * 30;
 			}
 			final var current = new Date();
