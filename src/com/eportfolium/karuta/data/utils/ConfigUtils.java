f0852767e81173c2954b8449ac5c45e52fe8a488
Nobry OUK <o.p.nobry@gmail.com>
2025-09-16 22:08:13 -0400

    * Have flag to disable direct url time checking * Remove max time cap

--- src/com/eportfolium/karuta/data/utils/ConfigUtils.java
+++ src/com/eportfolium/karuta/data/utils/ConfigUtils.java
@@ -125,14 +125,6 @@ public class ConfigUtils {
 		return INSTANCE;
 	}
 
-	public boolean getBooleanProperty(final String key) {
-		final var value = properties.getProperty(key);
-		if (value == null) {
-			return false;
-		}
-		return "Y".equals(value.toUpperCase());
-	}
-
 	public BuildInfo getBuildInfo() {
 		return buildInfo;
 	}
